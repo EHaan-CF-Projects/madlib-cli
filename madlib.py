cb9134922e21cca473a4afc70edd94a477ec882c
EvyHaan <evy.haan@gmail.com>
2019-02-20 21:22:42 -0800

    fiddled with ideas - no progress

--- madlib.py
+++ madlib.py
@@ -12,17 +12,29 @@ INTRO = """
 print(INTRO)
 
 
-def get_story_template():
+def get_story_keywords():
     """Function that retrieves the story template.
     """
     madlib_keywords = []
     with open('./assets/story_template', 'r') as rf:
-        with open('./assets/user_madlib.txt', 'w') as wf:
+        with open('./assets/madlib_keywords.txt', 'w') as wf:
             for line in rf:
                 template_keywords = re.findall('{([^}]*)}', line)
                 if template_keywords != ['']:
                     for keyword in template_keywords:
                         wf.write(keyword + '\n')
 
+
+def get_user_words():
+    """Function that asks for the user to enter silly words.
+    """
+    with open('.assets/madlib_keywords', 'r') as rf:
+        with open('.assets/story_template', 'w') as wf:
+            for line in rf:
+                user_word = input('Enter %s : ' % line)
+                re.sub('{([^}]*)}', user_word, str)
+
+
 if __name__ == "__main__":
-    get_story_template()
+    get_story_keywords()
+    get_user_words()
--- test_madlib.py
+++ test_madlib.py
@@ -1,6 +1,9 @@
+from madlib import get_story_keywords
+import pytest
 
 
+def test_get_keywords():
+    assert get_story_keywords
 
 
-
-# Test that the story template is open as a read file
+# Test that the get_story_keywords function exists
